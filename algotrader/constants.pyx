2ecbc76e945f188636f043525f74a961864b46f5
Bradley Arsenault <genixpro@gmail.com>
2022-07-07 12:31:50 -0400

    Added in a ton more symbols to the symbol list to get a better decorrelated portfolio

--- algotrader/constants.pyx
+++ algotrader/constants.pyx
@@ -22,9 +22,7 @@ correlationAnalysisMovingAveragePeriodDays = 5
 verboseOutput = False
 generateCharts = False
 
-symbolsToTrade = [
-    # Tech stocks
-    "MGC",
+techStocks = [
     "HOOD",
     "COIN",
     "AAPL",
@@ -83,3 +81,126 @@ symbolsToTrade = [
     "GPN",
     "MCHP"
 ]
+
+funds = [
+    "MGC", # Vanguard megacap (all large companies)
+    "XLB", # Materials
+    "XLE", # Energy
+    "XLC", # Communications
+    "XLY", # Consumer Discretionary
+    "XLP" # Consumer Staples
+    "XLF", # Financial
+    "XLV", # Healthcare
+    "XLI" # Industrial
+    "IFRA", # Infrastructure
+    "XLK", # Tech
+    "XLU", # Utitilies
+]
+
+
+commodity = [
+    "IAU", # Gold
+    "SLV", # Silver,
+    "USO", # Oil
+    "UCO", # Crude oil
+    "PPLT", # Platinum
+    "WEAT", # Wheat - high expense, hopefully can find better
+    "DBB", # Base metals
+    "PALL", # Palladium
+    "CORN", # Corn
+    "CPER", # Copper
+    "SOYB", # Soybeans
+    "CANE",  # Sugar
+    "BAL", # Cotton
+    "DBA", # Agriculture, broad
+]
+
+realEstate = [
+    "LAND", # Farm
+    "BXP", # Office
+    "PLD", # Industrial
+    "CPT", # Apartment
+    "MAA", # Apartment
+]
+
+bonds = [
+    "BLV", # Long term USA
+    "BSV", # Short term USA
+    "SPIB", # Long term corporate USA
+    "SPHY", # High yield bonds
+]
+
+international = [
+    "VWOB", # Emerging market bonds
+    "SCHE", # Emerging market stock
+    "VPL", # Pacific asia
+    "VGK", # Europe
+    "VT", # Total world
+]
+
+financial = [
+    "JPM", # JP morgan chase
+    "BAC", # Bank of america
+    "WFC", #wells fargo
+    "RY", # RBC canada
+    "MS", # Morgan stanley
+    "HSBC", # HSBC
+    "SCHW", # Charles Schwab
+    "TD", # Toronto dominion bank
+]
+
+energy = [
+    "XOM", # Exxon Mobil
+    "CVX", # Chevron
+    "RDS.A", # Royal dutch
+    "TTE", # Total energies
+    "COP", # Conoco phillips
+    "BP", # British petroleum
+    "EQNR", # Equinor
+    "ENB", # Enbridge
+    "CNQ", # Canadian natural resources
+]
+
+manufacturing = [
+    "AZN", # Astra Zeneca
+    "SNY", # Sanofi
+    "WOLF", # Wolfspeed
+    "VC", # Visteon
+]
+
+retail = [
+    "WMT", # Walmart
+    "HD", # Home depot
+    "COST", # Costco
+    "CVS", # CVS Pharmacy
+    "LOW", # Lowes
+    "TGT", # Target
+    "DLTR", # Dollar Tree
+    "AZO", # Auto Zone
+    "KR", # Kroger
+]
+
+consumer = [
+    "PG", # Procter & Gamble
+    "PM", # Phillip Morris
+    "UL", # Unilever
+    "EL", # Estee Lauder
+    "CL", # Colgate Palmolive
+]
+
+food = [
+    "HSY", # Hershey
+     "KO", # Coca cola
+    "PEP", # Pepsico
+    "BUD", # Anhauser busch
+    "DEO", # Diageo
+    "MDLZ", # Mondelez International
+    "MNST", # Monster Beverage
+    "KDP", # Dr Pepper
+    "KHC", # Kraft Heinz
+]
+
+symbolsToTrade = techStocks + funds + commodity + realEstate + bonds + \
+                 international + financial + energy + manufacturing + retail + \
+                 consumer + food
+
