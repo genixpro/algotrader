c3ab3ff3fd88ecdc0c6b6fd26b055684dee3d667
Bradley Arsenault <genixpro@gmail.com>
2022-07-07 14:06:12 -0400

    added in a sleep call to the code that downloads the latest data from ameritrade so we don't get rate limited

--- algotrader/bin/download_latest_data.py
+++ algotrader/bin/download_latest_data.py
@@ -3,12 +3,14 @@ pyximport.install()
 
 from algotrader import constants
 from algotrader import historical_data
+import time
 
 def run():
     for symbol in constants.symbolsToTrade:
         print(f"Downloading historical data for {symbol}")
         historicals = historical_data.HistoricalPrices()
         historicals.saveRawDataLocally(symbol)
+        time.sleep(0.5)
 
 
 
